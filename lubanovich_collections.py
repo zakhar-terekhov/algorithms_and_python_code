832296829e230667a4712281be2bea7c4c5ebf6f
owwwl666 <zaharsalam228@gmail.com>
2025-05-19 20:19:10 +0900

    moved files to lubanovich_python directory

--- lubanovich_collections.py
+++
@@ -1,7 +0,0 @@
-from collections import defaultdict,OrderedDict
-
-
-plain = {"a": 1, "b": 2, "c": 3}
-fancy = OrderedDict([("a",1),("b",2),("c",3)])
-dict_of_list = defaultdict(list)
-dict_of_list["a"].append("something")
